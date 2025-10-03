edf7760bd5159f3f3b4125cca01e4426977fa1c4
Noé <noe.schurtenberger@gmx.ch>
2025-10-03 08:33:36 +0200

    Aufgabe 1 gemacht

--- Aufgabe 1/Program.cs
+++ Aufgabe 1/Program.cs
@@ -10,6 +10,18 @@ namespace Aufgabe_1
     {
         static void Main(string[] args)
         {
+            int Zahl1;
+            int Zahl2;
+            int Summe;
+
+            Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+            Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+            Zahl2 = Convert.ToInt32(Console.ReadLine());
+            Summe = Zahl1 * Zahl2;
+            Console.WriteLine("Die Summe der beiden Zahlen ist: " + Summe);
+
+
         }
     }
 }
