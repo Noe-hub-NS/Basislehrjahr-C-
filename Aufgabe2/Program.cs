f440359313a7f9b4b7d1310c4666a377819d2cfb
Noé <noe.schurtenberger@gmx.ch>
2025-10-03 08:54:12 +0200

    Aufgabe 2 gemacht

---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie die Anzahl Tage ihres Monats an?");
+            string input = Console.ReadLine();
+            int zahl = 0;
+            bool isNumber = int.TryParse(input, out zahl);
+            
+            if (isNumber)
+            {
+                if (zahl >= 28 && zahl <= 31)
+                {
+                    Console.WriteLine("Der Monat hat " + (zahl * 86400 )+ " Sekunden.");
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Ein Monat hat zwischen 28 und 31 Tagen.");
+                }
+            }            
+        }
+    }
+}
